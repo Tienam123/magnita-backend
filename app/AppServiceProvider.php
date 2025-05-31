7ba3f4e83bee980e71cb3b7ffed0c8ad2ef3f352
Vladyslav Honchar <vladyslav.honchar@tangramltd.com>
2025-06-01 00:49:08 +0300

    chore(project): initialize project

---
+++ app/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use App\Magnita\Auth\AuthServiceProvider;
+use App\Magnita\Common\CommonServiceProvider;
+use App\Magnita\Common\Infrastructure\Providers\TelescopeServiceProvider;
+use App\Magnita\User\UserServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    private array $modules = [
+        UserServiceProvider::class,
+        AuthServiceProvider::class,
+        CommonServiceProvider::class,
+    ];
+
+    public function register():void
+    {
+        if (!$this->app->isProduction() && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
+    }
+
+    public function boot(): void
+    {
+        foreach ($this->modules as $module) {
+            $this->app->register($module);
+        }
+    }
+}
